060b144d6bed429c17bb0e1175323b9ff52bca67
Breno do Val <brenorvale@gmail.com>
2024-11-11 22:37:07 -0300

    feat(Boilerplate): add boilerplate

---
+++ src/main/java/com/learning_tracker/topic_service/TopicServiceApplication.java
@@ -0,0 +1,13 @@
+package com.learning_tracker.topic_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopicServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TopicServiceApplication.class, args);
+	}
+
+}
